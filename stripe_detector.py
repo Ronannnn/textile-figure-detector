ebfdbc5a2685ebf93d1f7197eaf06a27c43ce906
Chen Ruonan <ruonan.chen@joynext.com>
2021-09-11 14:44:11 +0800

    Refactor codes 1. remove invalid peak getter 2. remove unused funcs 3. remove local bash script

--- stripe_detector.py
+++ stripe_detector.py
@@ -3,7 +3,6 @@ import numpy as np
 from pathlib import Path
 import os
 
-from scipy.signal import find_peaks
 
 
 class StripeDetector:
@@ -19,19 +18,31 @@ class StripeDetector:
         self.gray_scaled_img = cv.cvtColor(self.contrast_enhanced_img, cv.COLOR_RGB2GRAY)
         self.background = self.shadow_weaken_img
 
-        self.thickness = 1
+        self.edge_thickness = 1
+        self.circle_thickness = -1
 
-    def draw_edges_with_canny(self, thresh1, thresh2, color=(255, 0, 0)):
+    @staticmethod
+    def detect_dir(target_dir):
+        for _, _, file_list in os.walk(target_dir):
+            for filename in file_list:
+                if "[" not in filename and "]" not in filename:  # since
+                    StripeDetector(os.path.join(target_dir, filename)).draw_circles_with_canny()
+
+    def draw_circles_with_canny(self):
+        edges = self.__get_edges_with_canny(thresh1=50, thresh2=200)
+        points = self.__find_coordinate(edges, interval=25)
+        self.__draw_circles(points)
+
+    def __get_edges_with_canny(self, thresh1, thresh2, color=(255, 0, 0)):
         edges = cv.Canny(self.gray_scaled_img, thresh1, thresh2)
         contours, _ = cv.findContours(edges, cv.RETR_TREE, cv.CHAIN_APPROX_NONE)
-        contours = self.__remove_invalid_edges(contours, 500)
         new_img = self.background.copy()
-        cv.drawContours(new_img, contours, -1, color, 2)
-        self.__show_and_save(new_img, "edges_with_canny")
+        cv.drawContours(new_img, contours, -1, color, self.edge_thickness)
+        self.__show_and_save(new_img, "with-edges", save=False)
         return edges
 
     # TODO: suppose stripes are vertical
-    def find_coordinate(self, edges, interval):
+    def __find_coordinate(self, edges, interval):
         h, _ = edges.shape
         cur_h = interval
         cloned_edges = np.array(edges)
@@ -39,60 +50,33 @@ class StripeDetector:
         points = []
         while cur_h < h:
             shadow_list = cv.reduce(cloned_edges[cur_h - interval: cur_h, :], 0, cv.REDUCE_SUM, dtype=cv.CV_32S)[0]
-            peak = self.__get_peak_1(shadow_list)
+            peak = self.__get_peak(shadow_list)
             peak = self.__merge_peak(peak)
-            # peak = [peak[i] for i in range(len(peak)) if i % 2 == 0]
             for x in peak:
                 points.append([x, cur_h])
             cur_h = cur_h + interval
         return points
 
-    def draw_circles(self, points):
+    def __draw_circles(self, points):
         new_img = self.raw_img.copy()
         for point in points:
-            new_img = cv.circle(new_img, (point[0], point[1]), radius=0, color=(0, 0, 255), thickness=-1)
-        self.__show_and_save(new_img, "canny_with_circles")
+            new_img = cv.circle(new_img, (point[0], point[1]), radius=0, color=(0, 0, 255), thickness=self.circle_thickness)
+        self.__show_and_save(new_img, "with-circles")
 
     @staticmethod
-    def __get_peak_1(shadow_list):
+    def __get_peak(shadow_list):
         shadow_list = np.array(shadow_list)
         threshold = int(np.max(shadow_list) * 0.25)
         return [i for i in range(len(shadow_list)) if shadow_list[i] > threshold]
 
-    @staticmethod
-    def __get_peak_2(shadow_list):
+    def __merge_peak(self, peak, thresh=1):
         """
-        Reference: https://www.cnblogs.com/ronny/p/3616470.html
-        """
-        # first order difference vector
-        vec = shadow_list[1:] - shadow_list[:-1]
-        vec = np.sign(vec)
-        if vec[len(vec) - 1] == 0:
-            vec[len(vec) - 1] = 1
-        for i in range(len(vec) - 2, 0, -1):
-            if vec[i] == 0:
-                if vec[i + 1] >= 0:
-                    vec[i] = 1
-                else:
-                    vec[i] = -1
-        vec = vec[1:] - vec[:-1]
-        peak = []
-        for i in range(len(vec) - 1):
-            if vec[i + 1] - vec[i] == -2:  # peak
-                peak.append(i + 1)
-        return peak
-
-    @staticmethod
-    def __get_peak_3(shadow_list):
-        peak, _ = find_peaks(shadow_list, height=0.95)
-        return peak
-
-    def __merge_peak(self, peak, thresh=5):
-        """
-        If the difference of abscissas of two points is smaller than 5, 
-        substitute these two points as their center
+        If the difference of abscissas of two points is smaller than threshold, 
+            substitute these two points as their center
         @param peak: one direction array, which store abscissas of points
         """
+        if len(peak) == 0:
+            return peak
         prev = peak[0]
         ret = []
         for i in range(1, len(peak)):
@@ -104,32 +88,8 @@ class StripeDetector:
                 prev = int((cur + prev) / 2)
         return ret
 
-    def draw_edges_with_structure(self, thresh, color=(255, 0, 0)):
-        _, img = cv.threshold(self.gray_scaled_img, thresh, 255, cv.THRESH_BINARY)  # 设定红色通道阈值210（阈值影响梯度运算效果）
-        kernel = cv.getStructuringElement(cv.MORPH_ELLIPSE, (9, 9))  # 定义矩形结构元素
-        dilated = cv.dilate(img, kernel)
-        contours, _ = cv.findContours(dilated.copy(), cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
-        new_img = self.background.copy()
-        cv.drawContours(new_img, contours, -1, color, self.thickness)
-        self.__show_and_save(new_img, "edges_with_structure")
-        return contours
-
-    @staticmethod
-    def __remove_invalid_edges(contours, invalid_thresh):
-        ret = []
-        for line in contours:
-            if (len(line) > invalid_thresh):
-                ret.append(line)
-        return ret
-
-    @staticmethod
-    def replenish_contours(contours):
-        print(len(contours))
-        # contours = np.vstack(contours).squeeze()
-        print(contours)
-
     # Ref: https://www.codenong.com/44752240/
-    def __weaken_shadow(self, img):
+    def __weaken_shadow(self, img, save=False):
         rgb_planes = cv.split(img)
         result_norm_planes = []
         for plane in rgb_planes:
@@ -139,30 +99,20 @@ class StripeDetector:
             norm_img = cv.normalize(diff_img, None, alpha=0, beta=255, norm_type=cv.NORM_MINMAX, dtype=cv.CV_8UC1)
             result_norm_planes.append(norm_img)
         result_norm = cv.merge(result_norm_planes)
-        self.__show_and_save(result_norm, "weaken-shadow")
+        self.__show_and_save(result_norm, "weaken-shadow", save=save)
         return result_norm
 
-    def __enhance_contrast(self, img):
+    def __enhance_contrast(self, img, save=False):
         dst = np.zeros_like(img)
         cv.normalize(img, dst, 0, 255, cv.NORM_MINMAX, cv.CV_8U)
-        self.__show_and_save(dst, "enhanced-contrast")
+        self.__show_and_save(dst, "enhanced-contrast", save=save)
         return dst
 
     def __show_and_save(self, img, label, show=False, save=True):
         cv.imshow(label, img) if show else None
-        cv.imwrite(os.path.join(self.parent_dir, self.fn_stem + "-" + label + self.fn_suffix), img) if save else None
-
-    @staticmethod
-    def __cv_join():
-        cv.waitKey(0)
-        cv.destroyAllWindows()
+        cv.imwrite(os.path.join(self.parent_dir, self.fn_stem + "[" + label + "]" + self.fn_suffix), img) if save else None
 
 
 if __name__ == '__main__':
-    sd = StripeDetector("img/stripe/2.png")
-    edges = sd.draw_edges_with_canny(thresh1=90, thresh2=100)
-    points = sd.find_coordinate(edges, interval=25)
-    sd.draw_circles(points)
-    # incomplete_contours = sd.draw_edges_with_structure(20)
-    # complete_contours = sd.replenish_contours(incomplete_contours)
-    # sd.remove_shadow_by_max_filtering()
+    # StripeDetector.detect_dir("img/stripe/raw")
+    StripeDetector("img/stripe/raw/a6.png").draw_circles_with_canny()
