262c7024d6efd15d8652a51d5149976b997325b1
Chen Ruonan <ruonan.chen@joynext.com>
2021-09-11 15:06:19 +0800

    Add parameters in ctor

--- stripe_detector.py
+++ stripe_detector.py
@@ -4,20 +4,34 @@ from pathlib import Path
 import os
 
 
-
 class StripeDetector:
-    def __init__(self, filename):
+    def __init__(
+        self,
+        filename,
+        canny_thresh1=50,
+        canny_thresh2=200,
+        interval=25,
+        merge_thresh=5
+    ):
+        # filename info
         filepath = Path(filename)
         self.parent_dir = filepath.parent.absolute()
         self.fn_stem = filepath.stem      # filename without parent dir and suffix
         self.fn_suffix = filepath.suffix  # suffix with dot, e.g. ".jpg"
 
+        # img preprocess
         self.raw_img = cv.imread(filename)
         self.shadow_weaken_img = self.__weaken_shadow(self.raw_img)
         self.contrast_enhanced_img = self.__enhance_contrast(self.shadow_weaken_img)
         self.gray_scaled_img = cv.cvtColor(self.contrast_enhanced_img, cv.COLOR_RGB2GRAY)
-        self.background = self.shadow_weaken_img
 
+        # input parameters
+        self.canny_thresh1 = canny_thresh1
+        self.canny_thresh2 = canny_thresh2
+        self.interval = interval
+        self.merge_thresh = merge_thresh
+
+        # built-in parameters
         self.edge_thickness = 1
         self.circle_thickness = -1
 
@@ -26,23 +40,29 @@ class StripeDetector:
         for _, _, file_list in os.walk(target_dir):
             for filename in file_list:
                 if "[" not in filename and "]" not in filename:  # since
-                    StripeDetector(os.path.join(target_dir, filename)).draw_circles_with_canny()
+                    StripeDetector(
+                        filename=os.path.join(target_dir, filename),
+                        canny_thresh1=50,
+                        canny_thresh2=200,
+                        interval=25,
+                        merge_thresh=5
+                    ).draw_circles_with_canny()
 
     def draw_circles_with_canny(self):
-        edges = self.__get_edges_with_canny(thresh1=50, thresh2=200)
-        points = self.__find_coordinate(edges, interval=25)
+        edges = self.__get_edges_with_canny(self.canny_thresh1, self.canny_thresh2)
+        points = self.__find_coordinates(edges, self.interval, self.merge_thresh)
         self.__draw_circles(points)
 
     def __get_edges_with_canny(self, thresh1, thresh2, color=(255, 0, 0)):
         edges = cv.Canny(self.gray_scaled_img, thresh1, thresh2)
         contours, _ = cv.findContours(edges, cv.RETR_TREE, cv.CHAIN_APPROX_NONE)
-        new_img = self.background.copy()
+        new_img = self.raw_img.copy()
         cv.drawContours(new_img, contours, -1, color, self.edge_thickness)
         self.__show_and_save(new_img, "with-edges", save=False)
         return edges
 
     # TODO: suppose stripes are vertical
-    def __find_coordinate(self, edges, interval):
+    def __find_coordinates(self, edges, interval, merge_thresh):
         h, _ = edges.shape
         cur_h = interval
         cloned_edges = np.array(edges)
@@ -51,25 +71,29 @@ class StripeDetector:
         while cur_h < h:
             shadow_list = cv.reduce(cloned_edges[cur_h - interval: cur_h, :], 0, cv.REDUCE_SUM, dtype=cv.CV_32S)[0]
             peak = self.__get_peak(shadow_list)
-            peak = self.__merge_peak(peak)
+            peak = self.__merge_peak(peak, merge_thresh)
             for x in peak:
                 points.append([x, cur_h])
             cur_h = cur_h + interval
         return points
 
-    def __draw_circles(self, points):
+    def __draw_circles(self, points, color=(0, 0, 255)):
         new_img = self.raw_img.copy()
         for point in points:
-            new_img = cv.circle(new_img, (point[0], point[1]), radius=0, color=(0, 0, 255), thickness=self.circle_thickness)
+            new_img = cv.circle(new_img, (point[0], point[1]), radius=0, color=color, thickness=self.circle_thickness)
         self.__show_and_save(new_img, "with-circles")
 
     @staticmethod
     def __get_peak(shadow_list):
+        """
+        @param shadow_list: one direction list
+        """
         shadow_list = np.array(shadow_list)
         threshold = int(np.max(shadow_list) * 0.25)
         return [i for i in range(len(shadow_list)) if shadow_list[i] > threshold]
 
-    def __merge_peak(self, peak, thresh=1):
+    @staticmethod
+    def __merge_peak(peak, merge_thresh):
         """
         If the difference of abscissas of two points is smaller than threshold, 
             substitute these two points as their center
@@ -81,15 +105,18 @@ class StripeDetector:
         ret = []
         for i in range(1, len(peak)):
             cur = peak[i]
-            if cur - prev > thresh:
+            if cur - prev > merge_thresh:
                 ret.append(prev)
                 prev = cur
             else:
                 prev = int((cur + prev) / 2)
         return ret
 
-    # Ref: https://www.codenong.com/44752240/
-    def __weaken_shadow(self, img, save=False):
+    @staticmethod
+    def __weaken_shadow(img):
+        """
+        Ref: https://www.codenong.com/44752240/
+        """
         rgb_planes = cv.split(img)
         result_norm_planes = []
         for plane in rgb_planes:
@@ -99,13 +126,12 @@ class StripeDetector:
             norm_img = cv.normalize(diff_img, None, alpha=0, beta=255, norm_type=cv.NORM_MINMAX, dtype=cv.CV_8UC1)
             result_norm_planes.append(norm_img)
         result_norm = cv.merge(result_norm_planes)
-        self.__show_and_save(result_norm, "weaken-shadow", save=save)
         return result_norm
 
-    def __enhance_contrast(self, img, save=False):
+    @staticmethod
+    def __enhance_contrast(img):
         dst = np.zeros_like(img)
         cv.normalize(img, dst, 0, 255, cv.NORM_MINMAX, cv.CV_8U)
-        self.__show_and_save(dst, "enhanced-contrast", save=save)
         return dst
 
     def __show_and_save(self, img, label, show=False, save=True):
@@ -114,5 +140,5 @@ class StripeDetector:
 
 
 if __name__ == '__main__':
-    # StripeDetector.detect_dir("img/stripe/raw")
-    StripeDetector("img/stripe/raw/a6.png").draw_circles_with_canny()
+    StripeDetector.detect_dir("img/stripe/raw")
+    StripeDetector("img/stripe/raw/a6.png", merge_thresh=1).draw_circles_with_canny()
